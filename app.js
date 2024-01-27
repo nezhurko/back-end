2e7612a04c4112d740d4764683b38feea7816f34
Tim <133401677+nezhurko@users.noreply.github.com>
2024-01-27 16:45:43 +0200

    mongo db init and logger

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+import loggerMiddleware from './middleware/logger.js';
 
 import healthCheckRoutes from './routes/healthCheckRoutes.js';
 
@@ -15,9 +18,39 @@ global.categories = [];
 
 app.use(express.json());
 
+app.use(loggerMiddleware);
+
 app.use(healthCheckRoutes);
 app.use('/user/',userRoutes);
 app.use('/category/',categoryRoutes);
 app.use('/record/',recordRoutes);
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).json({ error: 'Bad request.' });
+    } else if (err.type === 'entity.too.large') {
+        return res.status(413).json({ error: 'Payload too large.' });
+    }
+    return res.status(500).json({ error: 'Oops! Something went wrong.' });
+});
+
+(async () => {
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+    const host = process.env.MONGODB_HOST;
+    const dbName = process.env.MONGODB_DATABASE;
+
+    const connectionString = `mongodb+srv://${username}:${password}@${host}/${dbName}?retryWrites=true&w=majority`;
+
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB.');
+    } catch (error) {
+        console.error('Error connecting to MongoDB.');
+    }
+})();
 
 export default app;
